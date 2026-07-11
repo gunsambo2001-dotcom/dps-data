74d689a8fd39e0fbf2a9fa70a24498458250727c
gunsambo <gunsambo2001@gmail.com>
2026-07-12 00:48:21 +0800

    data

---
+++ data.js
@@ -0,0 +1,693 @@
+window.DPS = {
+ "updated": "00:50",
+ "updated_full": "2026-07-12 00:50",
+ "updated_ts": 1783788609,
+ "window_hours": 3,
+ "window": "21:50–00:50",
+ "messages": [
+  {
+   "time": "21:52",
+   "ts": 1783777920,
+   "author": "Saryuna",
+   "text": "как почтовка?сосновый бор?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "21:52",
+   "ts": 1783777920,
+   "author": "Так надо",
+   "text": "Тальцы",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "21:59",
+   "ts": 1783778340,
+   "author": "Артём Клименищев",
+   "text": "Ваи как?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:00",
+   "ts": 1783778400,
+   "author": "Петрович",
+   "text": "?",
+   "quote": "Так надо Тальцы",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:05",
+   "ts": 1783778700,
+   "author": "Так надо",
+   "text": "Тальцы 0 сосновый бор чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "22:27",
+   "ts": 1783780020,
+   "author": "Кирилл Коварский",
+   "text": "Как по кварталам ?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:34",
+   "ts": 1783780440,
+   "author": "Аюр Фёдоров",
+   "text": "Как комушка?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:39",
+   "ts": 1783780740,
+   "author": "Дарима Мункуева",
+   "text": "Мокрова,, мороженка чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "22:39",
+   "ts": 1783780740,
+   "author": "Антон",
+   "text": "С нижней иволги до города как?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:39",
+   "ts": 1783780740,
+   "author": "Bairovich",
+   "text": "148 квартал борт актив",
+   "quote": "",
+   "fwd": false,
+   "kind": "active",
+   "borts": []
+  },
+  {
+   "time": "22:41",
+   "ts": 1783780860,
+   "author": "Тимур",
+   "text": "А где именно",
+   "quote": "Bairovich 148 квартал борт актив",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:41",
+   "ts": 1783780860,
+   "author": "Тимур",
+   "text": "А импульс как",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "22:42",
+   "ts": 1783780920,
+   "author": "Юлия Лемяскина",
+   "text": "Как мясокомбинат, кольцо, почтовка?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:45",
+   "ts": 1783781100,
+   "author": "Кирилл Коварский",
+   "text": "Как 18кв",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:45",
+   "ts": 1783781100,
+   "author": "Кирилл Коварский",
+   "text": "Товары Бурятии",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "22:45",
+   "ts": 1783781100,
+   "author": "Кирилл Коварский",
+   "text": "Кольцо",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "22:48",
+   "ts": 1783781280,
+   "author": "Алексей",
+   "text": "Кольцо почтовка чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "22:49",
+   "ts": 1783781340,
+   "author": "Дарима Мункуева",
+   "text": "Пиплз как?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:50",
+   "ts": 1783781400,
+   "author": "Angelina",
+   "text": "Топаз рядом идет танцует мужчина в сторону Эрхирика по правой стороне",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "22:51",
+   "ts": 1783781460,
+   "author": "Игорь Комарицын",
+   "text": "Как труба Динамо типография борсоева сизо Наран левый пятый бурвод?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:52",
+   "ts": 1783781520,
+   "author": "Илья",
+   "text": "Как по 100 кВ кто знает подскажите",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:52",
+   "ts": 1783781520,
+   "author": "𝖘𝖊𝖗𝖌𝖊𝖊v𝖓𝖆",
+   "text": "Мёд чистый",
+   "quote": "Илья Как по 100 кВ кто знает подскажите",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "22:54",
+   "ts": 1783781640,
+   "author": "Илья",
+   "text": "А горький",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "22:55",
+   "ts": 1783781700,
+   "author": "Кирилл Коварский",
+   "text": "Где все пятнистые",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:55",
+   "ts": 1783781700,
+   "author": "Виктория",
+   "text": "Как сотниково?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:58",
+   "ts": 1783781880,
+   "author": "Tsyben",
+   "text": "Как гунны, Нур?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "22:59",
+   "ts": 1783781940,
+   "author": "Мария",
+   "text": "Нижняя иволга по федеральной чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "23:00",
+   "ts": 1783782000,
+   "author": "Мария",
+   "text": "Чисто",
+   "quote": "Tsyben Как гунны, Нур?",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "23:01",
+   "ts": 1783782060,
+   "author": "Tsyben",
+   "text": "Спасибо",
+   "quote": "Мария Чисто",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "23:02",
+   "ts": 1783782120,
+   "author": "Мария",
+   "text": "Картер чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "23:09",
+   "ts": 1783782540,
+   "author": "Владислав",
+   "text": "с оймура до с.сухой как?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:10",
+   "ts": 1783782600,
+   "author": "Евгений Балданов",
+   "text": "Как нижний саянтуй?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:11",
+   "ts": 1783782660,
+   "author": "Сарана",
+   "text": "Как центр, элеватор?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:12",
+   "ts": 1783782720,
+   "author": "Bairovich",
+   "text": "Во дворе кого то хлопнули",
+   "quote": "Тимур А где именно",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "23:12",
+   "ts": 1783782720,
+   "author": "Bairovich",
+   "text": "Не знаю",
+   "quote": "Тимур А импульс как",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "23:19",
+   "ts": 1783783140,
+   "author": "Сарана",
+   "text": "Чисто",
+   "quote": "Илья А горький",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "23:19",
+   "ts": 1783783140,
+   "author": "Баин Найданов",
+   "text": "Чисто",
+   "quote": "Илья Как по 100 кВ кто знает подскажите",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "23:20",
+   "ts": 1783783200,
+   "author": "Юлия Лемяскина",
+   "text": "Как 40-е квартала?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": [
+    "40"
+   ]
+  },
+  {
+   "time": "23:20",
+   "ts": 1783783200,
+   "author": "Баин Найданов",
+   "text": "Как 40-южный?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": [
+    "40"
+   ]
+  },
+  {
+   "time": "23:23",
+   "ts": 1783783380,
+   "author": "Жаргал Цыбиков",
+   "text": "Чисто",
+   "quote": "Сарана Как центр, элеватор?",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "23:28",
+   "ts": 1783783680,
+   "author": "Данил Балуев",
+   "text": "Катается борт Lada Vesta, тут по Сотниково кругами по улицам, недавно возле стадиона на первой стоял прижал кого то",
+   "quote": "Виктория Как сотниково?",
+   "fwd": false,
+   "kind": "active",
+   "borts": []
+  },
+  {
+   "time": "23:34",
+   "ts": 1783784040,
+   "author": "Виктория",
+   "text": "150 на пионере",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": [
+    "150"
+   ]
+  },
+  {
+   "time": "23:34",
+   "ts": 1783784040,
+   "author": "Алдар",
+   "text": "По поселью как?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:35",
+   "ts": 1783784100,
+   "author": "Алдар",
+   "text": "Исток солдатский?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:36",
+   "ts": 1783784160,
+   "author": "Жаргал Цыбиков",
+   "text": "Начало Солдатского чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "23:38",
+   "ts": 1783784280,
+   "author": "Артём",
+   "text": "Как левый ?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:41",
+   "ts": 1783784460,
+   "author": "Жаргал Цыбиков",
+   "text": "Зелень",
+   "quote": "Артём Как левый ?",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "23:41",
+   "ts": 1783784460,
+   "author": "Жаргал Цыбиков",
+   "text": "Борсоева карманы тоже были чистыми",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "23:41",
+   "ts": 1783784460,
+   "author": "Angelina",
+   "text": "190 по восточному катается в районе провижки",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": [
+    "190"
+   ]
+  },
+  {
+   "time": "23:41",
+   "ts": 1783784460,
+   "author": "Мария",
+   "text": "С нижней иволги до иволги чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "23:42",
+   "ts": 1783784520,
+   "author": "Дарима Мункуева",
+   "text": "Как 140-е?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": [
+    "140"
+   ]
+  },
+  {
+   "time": "23:42",
+   "ts": 1783784520,
+   "author": "Angelina",
+   "text": "На техасе рядом машину остановил",
+   "quote": "Angelina 190 по восточному катается в районе провижки",
+   "fwd": false,
+   "kind": "active",
+   "borts": []
+  },
+  {
+   "time": "23:45",
+   "ts": 1783784700,
+   "author": "Аюр Фёдоров",
+   "text": "Как 18 квартал",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:47",
+   "ts": 1783784820,
+   "author": "Юлия",
+   "text": "Аэропорт как?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:48",
+   "ts": 1783784880,
+   "author": "Юлия",
+   "text": "Как аэропорт?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "23:54",
+   "ts": 1783785240,
+   "author": "Бадма",
+   "text": "190 базар проехал в сторону города",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": [
+    "190"
+   ]
+  },
+  {
+   "time": "00:09",
+   "ts": 1783786140,
+   "author": "Так надо",
+   "text": "Как импульс спиртзавод",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "00:11",
+   "ts": 1783786260,
+   "author": "Ксения",
+   "text": "Как горький ,сотые ,102 кв",
+   "quote": "Переслано: Ксения",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "00:15",
+   "ts": 1783786500,
+   "author": "Нима Бадмаев",
+   "text": "Чисто с горького",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "00:15",
+   "ts": 1783786500,
+   "author": "Екатерина Русина",
+   "text": "Как вахмистрова?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "00:15",
+   "ts": 1783786500,
+   "author": "Нима Бадмаев",
+   "text": "Барис чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "00:15",
+   "ts": 1783786500,
+   "author": "Нима Бадмаев",
+   "text": "До меда",
+   "quote": "",
+   "fwd": false,
+   "kind": "other",
+   "borts": []
+  },
+  {
+   "time": "00:16",
+   "ts": 1783786560,
+   "author": "Костя Сутурин",
+   "text": "Стеколка двз как?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  },
+  {
+   "time": "00:24",
+   "ts": 1783787040,
+   "author": "Позитивчик",
+   "text": "Поселье чисто",
+   "quote": "",
+   "fwd": false,
+   "kind": "clean",
+   "borts": []
+  },
+  {
+   "time": "00:28",
+   "ts": 1783787280,
+   "author": "Пётр",
+   "text": "102 чисто",
+   "quote": "Ксения Как горький ,сотые ,102 кв",
+   "fwd": false,
+   "kind": "clean",
+   "borts": [
+    "102"
+   ]
+  },
+  {
+   "time": "00:34",
+   "ts": 1783787640,
+   "author": "Валерия",
+   "text": "Сотниково пост просто стоят",
+   "quote": "",
+   "fwd": false,
+   "kind": "active",
+   "borts": []
+  },
+  {
+   "time": "00:37",
+   "ts": 1783787820,
+   "author": "Алексей Родин",
+   "text": "Как Дивизка.?",
+   "quote": "",
+   "fwd": false,
+   "kind": "question",
+   "borts": []
+  }
+ ]
+};
